4944fdcfcbb025b8358dfedba464d85f8e958821
Nikolay Demidovez <nikolaydemidovez@gmail.com>
2024-09-17 22:19:50 +0300

    added ship details

---
+++ Assets/App/Scripts/ShipDetail/ShipDetail.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace App.Scripts.ShipDetail
+{
+    public class ShipDetail : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _layerMaskAllowedEntities;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (((1 << other.gameObject.layer) & _layerMaskAllowedEntities) != 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
