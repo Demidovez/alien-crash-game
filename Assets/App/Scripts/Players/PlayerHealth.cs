3ebdd2470c32e33c3f5b7d964f51cf32ee1572d8
Nikolay Demidovez <nikolaydemidovez@gmail.com>
2024-10-05 10:53:38 +0300

    added anims hitting and dying

--- Assets/App/Scripts/Players/PlayerHealth.cs
+++ Assets/App/Scripts/Players/PlayerHealth.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using App.Scripts.HealthPills;
+using App.Scripts.Infrastructure;
 using App.Scripts.UI;
 using UnityEngine;
 
@@ -7,14 +9,24 @@ namespace App.Scripts.Players
 {
     public class PlayerHealth: IDisposable
     {
+        public Action OnTookDamageEvent;
+        public Action OnDeadEvent;
+        
         private readonly PlayerInfoUI _playerInfoUI;
         private readonly PopupManager _popupManager;
+        private readonly AsyncProcessor _asyncProcessor;
+        private const float DeathDelay = 2f;
         private float _health = 100;
 
-        public PlayerHealth(PlayerInfoUI playerInfoUI, PopupManager popupManager)
+        public PlayerHealth(
+            PlayerInfoUI playerInfoUI, 
+            PopupManager popupManager,
+            AsyncProcessor asyncProcessor
+        )
         {
             _playerInfoUI = playerInfoUI;
             _popupManager = popupManager;
+            _asyncProcessor = asyncProcessor;
 
             HealthPill.OnCollectedHealthPill += CollectedHealthPill;
         }
@@ -31,6 +43,11 @@ namespace App.Scripts.Players
 
         private void UpdateHealth(float value)
         {
+            if (_health <= 0)
+            {
+                return;
+            }
+            
             _health += value;
             _health = Mathf.Min(100, _health);
             
@@ -38,8 +55,19 @@ namespace App.Scripts.Players
 
             if (_health <= 0)
             {
-                _popupManager.ShowGameOver();
+                OnDeadEvent?.Invoke();
+                _asyncProcessor.StartCoroutine(Death());
             }
+            else
+            {
+                OnTookDamageEvent?.Invoke();
+            }
+        }
+        
+        IEnumerator Death()
+        {
+            yield return new WaitForSeconds(DeathDelay);
+            _popupManager.ShowGameOver();
         }
 
         public void Dispose()
