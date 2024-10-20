609e1fce536c28efdbf40179365f3fc6bb1dd388
Nikolay Demidovez <nikolaydemidovez@gmail.com>
2024-10-20 19:40:42 +0300

    added levels icons

--- Assets/App/Scripts/UI/MenuManager.cs
+++ Assets/App/Scripts/UI/MenuManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace App.Scripts.UI
 {
@@ -7,9 +8,40 @@ namespace App.Scripts.UI
     {
         public event Action<string> OnLoadLevelEvent;
 
+        [Header("Icons")]
+        public Sprite MusicOnIcon;
+        public Sprite MusicOffIcon;
+        public Sprite SoundOnIcon;
+        public Sprite SoundOffIcon;
+
+        [Header("Buttons")]
+        public Button MusicButton;
+        public Button SoundButton;
+        public Button ContinueButton;
+        public Button StartButton;
+        public Button LevelsButton;
+        public Button ExitButton;
+
         private void Awake()
         {
             gameObject.SetActive(false);
+
+            MusicButton.onClick.AddListener(OnMusicClick);
+            SoundButton.onClick.AddListener(OnSoundClick);
+            ContinueButton.onClick.AddListener(OnContinueClick);
+            StartButton.onClick.AddListener(OnStartClick);
+            LevelsButton.onClick.AddListener(OnLevelsClick);
+            ExitButton.onClick.AddListener(OnExitClick);
+        }
+
+        private void OnDestroy()
+        {
+            MusicButton.onClick.RemoveListener(OnMusicClick);
+            SoundButton.onClick.RemoveListener(OnSoundClick);
+            ContinueButton.onClick.RemoveListener(OnContinueClick);
+            StartButton.onClick.RemoveListener(OnStartClick);
+            LevelsButton.onClick.RemoveListener(OnLevelsClick);
+            ExitButton.onClick.RemoveListener(OnExitClick);
         }
 
         public void ShowMenu()
@@ -17,16 +49,41 @@ namespace App.Scripts.UI
             Time.timeScale = 0;
             gameObject.SetActive(true);
         }
-        
+
         public void HideMenu()
         {
             Time.timeScale = 1;
             gameObject.SetActive(false);
         }
 
-        public void LoadLevel()
+        private void OnMusicClick()
         {
-            OnLoadLevelEvent?.Invoke("Level_1");
+            Debug.Log("OnMusicClick");
+        }
+        
+        private void OnExitClick()
+        {
+            Debug.Log("OnExitClick");
+        }
+
+        private void OnLevelsClick()
+        {
+            Debug.Log("OnLevelsClick");
+        }
+
+        private void OnStartClick()
+        {
+            Debug.Log("OnStartClick");
+        }
+
+        private void OnContinueClick()
+        {
+            Debug.Log("OnContinueClick");
+        }
+
+        private void OnSoundClick()
+        {
+            Debug.Log("OnSoundClick");
         }
     }
 }
