dbafcf947dbd6f9996601762713a9f8b8cb572b4
Nikolay Demidovez <nikolaydemidovez@gmail.com>
2024-11-03 20:51:01 +0300

    added enemies sounds

---
+++ Assets/App/Scripts/Enemies/EnemyAudio.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+using Random = UnityEngine.Random;
+
+namespace App.Scripts.Enemies
+{
+    public class EnemyAudio: IEnemyAudio, ILateTickable, IDisposable
+    {
+        private static readonly int Footstep = Animator.StringToHash("Footstep");
+        
+        private readonly EnemyAudioSource _audio;
+        private readonly IEnemyHealth _enemyHealth;
+        private readonly Animator _animator;
+
+        private bool _isPlayingMoving;
+        private float _lastFootStep;
+        private float _lastTimeDamage;
+
+        public EnemyAudio(
+            EnemyAudioSource audioSource,
+            IEnemyHealth enemyHealth,
+            Animator animator
+        )
+        {
+            _audio = audioSource;
+            _enemyHealth = enemyHealth;
+            _animator = animator;
+            
+            _enemyHealth.OnTookDamageEvent += DamageSound;
+            _enemyHealth.OnConcussionEvent += ConcussionSound;
+        }
+
+        public void Dispose()
+        {
+            _enemyHealth.OnTookDamageEvent -= DamageSound;
+            _enemyHealth.OnConcussionEvent -= ConcussionSound;
+        }
+
+        public void LateTick()
+        {
+            RunningSound();
+        }
+        
+        private void ConcussionSound()
+        { 
+            _audio.Source.PlayOneShot(_audio.Clips.Concussion, 1f);
+        }
+
+        private void DamageSound(Transform obj)
+        {
+            var currentTime = Time.realtimeSinceStartup;
+
+            if (currentTime - _lastTimeDamage > 0.75f)
+            {
+                _audio.Source.PlayOneShot(_audio.Clips.Damage, 0.3f);
+                _lastTimeDamage = currentTime;
+            }
+        }
+
+        private void RunningSound()
+        {
+            var footStep = _animator.GetFloat(Footstep);
+
+            if (Mathf.Abs(footStep) < 0.00001f)
+            {
+                footStep = 0f;
+            }
+
+            if (_lastFootStep > 0 && footStep < 0 || _lastFootStep < 0 && footStep > 0)
+            {
+                var step = _audio.Clips.FootsSteps[Random.Range(0, _audio.Clips.FootsSteps.Length)];
+                
+                _audio.Source.PlayOneShot(step, 0.8f);
+            }
+
+            _lastFootStep = footStep;
+        }
+    }
+}
---
+++ Assets/App/Scripts/Enemies/EnemyAudioClips.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace App.Scripts.Enemies
+{
+    [Serializable]
+    public struct EnemyAudioClips
+    {
+        public AudioClip Damage;
+        public AudioClip Concussion;
+        public AudioClip[] FootsSteps;
+    }
+}
